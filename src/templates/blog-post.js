902281402a603f37a7ecfa4aad7ccadb8480b2dd
Nils Westhoff <nwesthoff@conceptivate.nl>
2019-11-20 00:20:09 +0100

    💥Remove date

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -28,16 +28,6 @@ class BlogPostTemplate extends React.Component {
         >
           {post.frontmatter.subtitle}
         </h2>
-        <p
-          style={{
-            ...scale(-1 / 5),
-            display: `block`,
-            marginBottom: rhythm(1),
-            marginTop: rhythm(-1),
-          }}
-        >
-          {post.frontmatter.date}
-        </p>
         <MDXRenderer>{post.body}</MDXRenderer>
       </Layout>
     )
